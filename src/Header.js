4568fdfcc1b18854a42a33a823957aefb8a712fb
jay winebrenner <jaywinebrenner@jays-iMac-2.local>
2020-09-18 22:41:15 -0700

    basic header

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import MenuIcon from "@material-ui/icons/Menu";
 import logo from './media/logoText.png'
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Avatar from "@material-ui/core/Avatar"
+
 
 function Header() {
   return (
     <div className="header">
-      <h1>I am a header</h1>
-      <MenuIcon/>
-      <img className="header__logoText" src={logo}/>
+      <div className="header__left">
+       <MenuIcon />
+      <img className="header__logoText" src={logo} alt="logo" />
+      </div>
 
-      
+
+      <div className="header__inputContainer">
+        <input type="text" />
+      <SearchIcon />
+      </div>
+      <div className="header__icons">
+      <VideoCallIcon />
+      <AppsIcon />
+      <NotificationsIcon />
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
